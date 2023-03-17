d59bfdcf74b7749fbf1a5cb1a0cc10e8d5830b4d
Patrick Brito <patrickbritokun530@gmail.com>
2023-03-17 17:14:14 -0300

    projeto escolher usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{  path: '',
+  component: UsuarioComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
